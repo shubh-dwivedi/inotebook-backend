ceadd56e65a83c1aa804ab4860a6d0fc0ded5f45
Shubham Dwivedi <51696142+shubh-dwivedi@users.noreply.github.com>
2022-08-30 20:42:41 +0530

    testing

--- middleware/fetchuser.js
+++ middleware/fetchuser.js
@@ -4,16 +4,13 @@ const JWT_SECRET = "shubhamisagoodb$oy";
 const fetchuser = (req, res, next) => {
     const token = req.header('auth-token');
     if(!token) {
-        console.log(JSON.stringify(req.headers));
         res.status(401).send({error: "Please provide a token to get authorised"});
     } else {
         try {
             const data = jwt.verify(token, JWT_SECRET);
             req.user = data.user;
-            console.log(token);
             next();
         } catch (error) {
-            console.log(token);
             res.status(401).send({error: "Please authenticate using a valid token. " + error});
         }
     }
