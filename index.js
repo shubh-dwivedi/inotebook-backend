9030d9971debec06158260a6f5eff05fc66ca534
Shubham Dwivedi <51696142+shubh-dwivedi@users.noreply.github.com>
2022-08-30 17:58:19 +0530

    testing

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const connectToMongo = require('./db');
 const express = require('express')
+const bodyParser = require('body-parser');
 var cors = require('cors')
 
 connectToMongo();
@@ -9,6 +10,11 @@ let server = require('http').Server(app);
 const port = process.env.PORT || 5000
 
 app.use(cors())
+
+// Configuring body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+
 app.use(express.json())
 
 app.use('/api/auth', require('./routes/auth'))
