ba645a335f6379241703b18c89a7c82905cb152f
Shubham Dwivedi <51696142+shubh-dwivedi@users.noreply.github.com>
2022-08-30 20:04:57 +0530

    testing

--- middleware/fetchuser.js
+++ middleware/fetchuser.js
@@ -4,7 +4,7 @@ const JWT_SECRET = "shubhamisagoodb$oy";
 const fetchuser = (req, res, next) => {
     const token = req.header('auth-token');
     if(!token) {
-        res.status(401).send({error: "Please provide a token to get authorised access"});
+        // res.status(401).send({error: "Please provide a token to get authorised access"});
     } else {
         try {
             const data = jwt.verify(token, JWT_SECRET);
