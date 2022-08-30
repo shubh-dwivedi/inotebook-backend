bea10894eea089a456bfb3fad1079aa4582cb05e
Shubham Dwivedi <51696142+shubh-dwivedi@users.noreply.github.com>
2022-08-30 17:06:36 +0530

    testing

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ var cors = require('cors')
 connectToMongo();
 
 const app = express()
-const port = 8080 || 5000
+let server = require('http').Server(app);
+const port = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
@@ -13,6 +14,6 @@ app.use(express.json())
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`iNotebook backend listening on port ${port}`);
 })
